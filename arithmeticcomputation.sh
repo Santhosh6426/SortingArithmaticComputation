5867138e736fe0ddc12bbf10a44bab81bc966133
Santhosh V <santhosh.v6426@gmail.com>
2020-09-10 10:52:53 +0530

    Added:Added program to compute a+b*c

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -3,4 +3,5 @@ echo "Welcome to Arithmetic Computation and Sorting program"
 read -p "Enter the value of first input 'a': " a
 read -p "Enter the value of second input 'b': " b
 read -p "Enter the value of third input 'c': " c
-
+w=$(( a + b * c ))
+echo "$a + $b * $c = $w"
