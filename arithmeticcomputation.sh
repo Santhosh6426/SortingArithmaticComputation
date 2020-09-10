5e17584f310dedfb5fc4e3dbafa75a5dbb03bd0f
Santhosh V <santhosh.v6426@gmail.com>
2020-09-10 10:39:10 +0530

    Added:Added program to take three inputs

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -1,1 +1,6 @@
 echo "Welcome to Arithmetic Computation and Sorting program"
+#!/bin/bash
+read -p "Enter the value of first input 'a': " a
+read -p "Enter the value of second input 'b': " b
+read -p "Enter the value of third input 'c': " c
+
