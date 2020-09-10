43e8d95223c94b489489f1dadf941dfcd8a08f79
Santhosh V <santhosh.v6426@gmail.com>
2020-09-10 12:33:00 +0530

    Added:Added the program to sort the Array in descending order.

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -14,7 +14,7 @@ echo "$a % $b + $c = $z"
 declare -A compute
 compute[first]=$w
 compute[second]=$x
-compute[third]=$y
+compute[third]=`awk 'BEGIN{printf("%.0f", '$y' )}'`
 compute[fourth]=$z
 echo -e "keys:${!compute[@]}\nValues:${compute[@]}"
 m=0
@@ -24,3 +24,19 @@ do
 	m=$(( $m + 1 ))
 done
 echo "Array:${arr[@]}"
+temp=${#arr[@]}
+for (( i=0; i<$temp; i++ ))
+do
+	for (( j=$(( $i + 1 )); j<=$temp; j++ ))
+	do
+		if [[ ${arr[$i]} -gt ${arr[$j]} ]]
+		then
+			continue
+		else
+			temp2=${arr[$i]}
+			arr[$i]=${arr[$j]}
+			arr[$j]=$temp2
+		fi
+	done
+done
+echo "Array sorted in Descending order : ${arr[@]}"
